be0721bcef210f55cbce12b48cefd1841b779862
TsingM <tsingm@foxmail.com>
2020-02-10 19:01:51 +0800

    nn2 && .md

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # PyTorch
-学习PyTorch
+
+## 开始
+
+### 环境
+
+MacOS + Anaconda(Python3.7) + PyTorch + VSCode
+
+### 简介
+
+Torch是一个有大量机器学习算法支持的科学计算框架，是一个与Numpy类似的张量（Tensor） 操作库，其特点是特别灵活，但因其采用了小众的编程语言是Lua，所以流行度不高，这也就有了PyTorch的出现。所以其实Torch是 PyTorch的前身，它们的底层语言相同，只是使用了不同的上层包装语言。
+PyTorch是一个基于Torch的Python开源机器学习库，用于自然语言处理等应用程序。它主要由Facebookd的人工智能小组开发。
+
+### 特点
+
+* 支持GPU
+
+* 灵活，支持动态神经网络
+
+* 底层代码易于理解
+
+* 命令式体验
+
+* 自定义扩展
+
+## 自动微分
+
+autograd 包是 PyTorch 中所有神经网络的核心。该包为 Tensors 上的所有操作提供自动微分。它是一个由运行定义的框架，这意味着以代码运行方式定义你的后向传播，并且每次迭代都可以不同。
+
+## 神经网络
+
+### 神经网络的训练步骤
+
+1. 定义一个神经网络
+
+2. 处理输入，调用反向传播
+
+3. 计算损失
+
+4. 更新参数
+
+### 神经网络的常见类
+
+* torch.Tensor - A multi-dimensional array with support for autograd operations like backward(). Also holds the gradient w.r.t. the tensor.
+支持自动微分操作（反向传播等）的多维数组
+
+* nn.Module - Neural network module. Convenient way of encapsulating parameters, with helpers for moving them to GPU, exporting, loading, etc.
+神经网络模块，封装参数用于GPU、导出、加载等
+
+* nn.Parameter - A kind of Tensor, that is automatically registered as a parameter when assigned as an attribute to a Module.
+一种张量，当作为属性分配给模块时，自动注册为一个参数
+
+* autograd.Function - Implements forward and backward definitions of an autograd operation. Every Tensor operation, creates at least a single Function node, that connects to functions that created a Tensor and encodes its history.
+实现自动微分操作的定义，每个张量操作至少生成一个节点
--- nn.py
+++ nn.py
@@ -3,26 +3,35 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Net(nn.Module):
+    # Net的初始化函数，定义神经网络的组成
     def __init__(self):
+        # 复制并使用Net父类的初始化方法，即先运行nn.Module的初始化函数
         super(Net, self).__init__()
-        # 定义卷积层（输入维度，输出维度，卷积核）
+        # 定义卷积层：输入维度（初始图像/6张特征图），输出维度（6/16张特征图），卷积核（5*5）
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        # 定义线性全连接函数（输入维度，输出维度）
+        # 定义全连接层：线性函数（输入维度，输出维度）
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
     
+    # 向前传播函数，定义神经网络的结构，反向传播函数基于此自动生成（autograd）
     def forward(self, x):
-        # 定义池化层（输入，窗口尺寸）
+        # 1.x经过卷积conv1和激活函数ReLU后，使用2*2的窗口进行最大池化，更新
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        # 2.x经过卷积conv2和激活函数ReLU后，使用2*2的窗口进行最大池化，更新
         x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        # 3.x经过view()由张量转换成一维向量（调用num_flat_features）
         x = x.view(-1, self.num_flat_features(x))
+        # 4.x经过全连接1和激活函数ReLU，更新
         x = F.relu(self.fc1(x))
+        # 5.x经过全连接2和激活函数ReLU，更新
         x = F.relu(self.fc2(x))
+        # 6.x经过全连接3，更新
         x = self.fc3(x)
         return x
 
+    # 计算张量x总特征量
     def num_flat_features(self, x):
         size = x.size()[1:]
         num_features = 1
@@ -32,3 +41,17 @@ class Net(nn.Module):
 
 net = Net()
 print(net)
+
+# 查看参数数量与conv1的尺寸
+params = list(net.parameters())
+print(len(params))
+print(params[0].size())
+
+# 随机生成32*32的输入
+input = torch.randn(1, 1, 32, 32)
+out = net(input)
+print(out)
+
+# 所有参数梯度缓存置零，用随机梯度进行反向传播
+net.zero_grad()
+out.backward(torch.randn(1, 10))
